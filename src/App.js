eb66e0e00b2a8bdb8d3580b232790c25bc277735
Lu Cho-Ching <choching.work@gmail.com>
2017-04-10 16:45:42 +0800

    add bold button

--- src/App.js
+++ src/App.js
@@ -1,77 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
-import 'draft-js-inline-toolbar-plugin/lib/plugin.css'; 
 
-import {EditorState} from 'draft-js';
+import {Editor, EditorState, RichUtils} from 'draft-js';
+import LinkEditor from './LinkEditor';
 
-import Editor, { createEditorStateWithText } from 'draft-js-plugins-editor';
-// eslint-disable-next-line import/no-unresolved
-import createInlineToolbarPlugin, { Separator } from 'draft-js-inline-toolbar-plugin';
-import {
-  ItalicButton,
-  BoldButton,
-  UnderlineButton,
-  CodeButton,
-  HeadlineOneButton,
-  HeadlineTwoButton,
-  HeadlineThreeButton,
-  UnorderedListButton,
-  OrderedListButton,
-  BlockquoteButton,
-  CodeBlockButton,
-} from 'draft-js-buttons'; // eslint-disable-line import/no-unresolved
-import editorStyles from './editorStyles.css';
-
-const inlineToolbarPlugin = createInlineToolbarPlugin({
-  structure: [
-    BoldButton,
-    ItalicButton,
-    UnderlineButton,
-    CodeButton,
-    Separator,
-    HeadlineOneButton,
-    HeadlineTwoButton,
-    HeadlineThreeButton,
-    UnorderedListButton,
-    OrderedListButton,
-    BlockquoteButton,
-    CodeBlockButton,
-  ]
-});
-
-const { InlineToolbar } = inlineToolbarPlugin;
-const plugins = [inlineToolbarPlugin];
-const text = 'In this editor a toolbar shows up once you select part of the text …';
-
- class MyEditor extends Component {
+class MyEditor extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      editorState: createEditorStateWithText(text),
-    }
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = (editorState) => this.setState({editorState});
+    this.handleBold = this.handleBold.bind(this);
   }
-
-  onChange = (editorState) => {
-    this.setState({
-      editorState,
-    });
-  };
-
-  focus = () => {
-    this.editor.focus();
-  };
+  
+  handleBold() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
 
   render() {
     return (
-      <div className={editorStyles.editor} onClick={this.focus}>
-        <Editor
-          editorState={this.state.editorState}
-          onChange={this.onChange}
-          plugins={plugins}
-          ref={(element) => { this.editor = element; }}
-        />
-        <InlineToolbar />
+      <div>
+        <button onClick={this.handleBold}>Bold</button>
+        <Editor 
+          editorState={this.state.editorState} 
+          onChange={this.onChange} />
       </div>
+        
     );
   }
 }
