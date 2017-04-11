414ad6e5471357d998c813d9d9d73db59207308d
Lu Cho-Ching <choching.work@gmail.com>
2017-04-11 10:27:56 +0800

    Add block style buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,69 @@
 import React, { Component } from 'react';
 import './App.css';
+import './MyEditor.css';
 
 import {Editor, EditorState, RichUtils, Modifier} from 'draft-js';
 import LinkEditor from './LinkEditor';
+
+class StyleButton extends Component {
+  constructor() {
+    super();
+    this.onToggle = (e) => {
+      e.preventDefault();
+      this.props.onToggle(this.props.style);
+    };
+  }
+  render() {
+    let className = 'MyEditor-styleButton';
+    if (this.props.active) {
+      className += ' MyEditor-activeButton';
+    }
+
+    return ( 
+      <span 
+        className={className}
+        onMouseDown={this.onToggle}
+      > 
+        {this.props.label} 
+      </span>
+     );
+  }
+}
+
+const BLOCK_TYPES = [
+  {label: 'H1', style: 'header-one'},
+  {label: 'H2', style: 'header-two'},
+  {label: 'H3', style: 'header-three'},
+  {label: 'H4', style: 'header-four'},
+  {label: 'H5', style: 'header-five'},
+  {label: 'H6', style: 'header-six'},
+  {label: 'Blockquote', style: 'blockquote'},
+  {label: 'UL', style: 'unordered-list-item'},
+  {label: 'OL', style: 'ordered-list-item'},
+  {label: 'Code Block', style: 'code-block'},
+];
+
+const BlockStyleControls = ({editorState, onToggle}) => {
+  const selection = editorState.getSelection();
+  const blockType = editorState
+    .getCurrentContent()
+    .getBlockForKey(selection.getStartKey())
+    .getType();
+
+  return (
+    <div>
+      {BLOCK_TYPES.map((type) =>
+        <StyleButton 
+          key={type.label}
+          active={type.style === blockType}
+          label={type.label}
+          onToggle={onToggle}
+          style={type.style}
+        />
+      )}
+    </div>
+  );
+}
 
 class MyEditor extends Component {
   constructor(props) {
@@ -12,8 +73,18 @@ class MyEditor extends Component {
     this.focus = () => this.refs.editor.focus();
     this.onTab = this.onTab.bind(this);
     this.handleBold = this.handleBold.bind(this);
+    this.toggleBlockType = (type) => this._toggleBlockType(type);
   }
   
+  _toggleBlockType(blockType) {
+    this.onChange(
+      RichUtils.toggleBlockType(
+        this.state.editorState,
+        blockType
+      )
+    );
+  }
+
   handleBold() {
     this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
   }
@@ -35,16 +106,25 @@ class MyEditor extends Component {
   }
 
   render() {
+    const editorState = this.state.editorState;
+
     return (
-      <div>
-        <button onClick={this.handleBold}>Bold</button>
-        <Editor 
-          editorState={this.state.editorState} 
-          onChange={this.onChange} 
-          onTab={this.onTab}
-          placeholder="寫點東西..."
-          ref="editor"  
+      <div className='MyEditor-root'>
+        {/*<button onClick={this.handleBold}>Bold</button>*/}
+        <BlockStyleControls 
+          editorState={editorState}
+          onToggle={this.toggleBlockType}
         />
+        <div className='MyEditor-editor'  onClick={this.focus}>
+          <Editor
+            editorState={editorState} 
+            onChange={this.onChange} 
+            onTab={this.onTab}
+            placeholder="寫點東西..."
+            ref="editor"  
+          />
+        </div>
+
       </div>
         
     );
